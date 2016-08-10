0865444434e0952973b9ecef6df5d2cfce088068
Danny Prikazsky <dannyprikaz@gmail.com>
2016-08-09 18:40:22 -0700

    added custom estimator and pr_curve plotting

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -1,4 +1,4 @@
-from sklearn.base import TransformerMixin
+from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
@@ -7,12 +7,16 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.cross_validation import train_test_split
 from sklearn.grid_search import GridSearchCV
-from sklearn.metrics import recall_score, precision_score, fbeta_score, accuracy_score
+from sklearn.metrics import recall_score, precision_score, fbeta_score, accuracy_score, precision_recall_curve
 from statsmodels.discrete.discrete_model import Logit
 from time import time
 import pandas as pd
 import numpy as np
 import cPickle as pickle
+import matplotlib.pyplot as plt
+pd.options.mode.chained_assignment = None
+
+beta = 1
 
 def tag_counter(lst, tag_values):
     tot = 0
@@ -153,12 +157,22 @@ class ScaleOrNo(CustomMixin):
             self.scaler.transform(X)
         return X
 
+class PickEstimator(BaseEstimator):
+    def __init__(self, estimator=AdaBoostClassifier()):
+        self.estimator = estimator
+
+    def fit(self, X, y):
+        self.estimator.fit(X,y)
+        return self
+
+    def predict(self, X):
+        return self.estimator.predict(X)
+
+    def predict_proba(self, X):
+        return self.estimator.predict_proba(X)
+
 
 def oversample(X, y):
-    columns = X.columns
-    name = y.name
-    X = X.values
-    y = y.values
     if y.sum() * 1.0 / y.size > .5:
         minority = 0
         sample_add = y.sum() * 2 - y.size
@@ -166,23 +180,26 @@ def oversample(X, y):
         minority = 1
         sample_add = y.size - y.sum() * 2
 
-    print 'Minority Class: ', minority
-    print 'Samples to Add: ', sample_add
-    minority_index = np.arange(y.size)[y == minority]
-    print 'Oversampling...'
-    start = time()
-    for i in xrange(sample_add):
-        index = np.random.choice(minority_index)
-        if i%1000 == 0 and i != 0:
-            print i, 'added.\nElapsed Time: ', time() - start, 'seconds'
-        X = np.append(X, X[index].reshape((1, len(columns))), axis=0)
-        y = np.append(y, minority)
-    X = pd.DataFrame(X)
-    X.columns = columns
-    y = pd.Series(y)
-    y.name = name
+    X_overs = X[y==1].sample(sample_add, replace=True)
+    y_overs = y[y==1].sample(sample_add, replace=True)
+    X = pd.concat([X, X_overs])
+    y = pd.concat([y, y_overs])
+    # minority_index = np.arange(y.size)[y == minority]
+    # print 'Oversampling...'
+    # start = time()
+    # for i in xrange(sample_add):
+    #     index = np.random.choice(minority_index)
+    #     if i%1000 == 0 and i != 0:
+    #         print i, 'added.\nElapsed Time: ', time() - start, 'seconds'
+    #     X = np.append(X, X[index].reshape((1, len(columns))), axis=0)
+    #     y = np.append(y, minority)
+    # X = pd.DataFrame(X)
+    # X.columns = columns
+    # y = pd.Series(y)
+    # y.name = name
 
     return X, y
+
 
 def get_train_test_sets(p, df, y):
     X_train, X_test, y_train, y_test = train_test_split(df, y, random_state=42)
@@ -227,16 +244,16 @@ def big_grid_search(training_sets, test_sets):
         best_estimator = {}
         all_ests = {}
         for classifier in classifier_names:
-            print classifiers[classifier].__name__
+            print '\n', classifiers[classifier].__name__
             for key, val in training_sets.iteritems():
-                print 'Training set: ', key
-                gs = GridSearchCV(classifiers[classifier](), param_grids[classifier], verbose=1, scoring=my_fbeta)
+                print '\nTraining set: ', key
+                gs = GridSearchCV(classifiers[classifier](), param_grids[classifier], verbose=3, scoring=my_fbeta)
                 gs.fit(*val)
-                print "CV FBeta: ", gs.best_score_
+                print "\nCV FBeta: ", gs.best_score_
                 all_ests[(key, classifier)] = gs.best_estimator_
                 preds = gs.best_estimator_.predict(test_sets[key][0])
                 y_true = test_sets[key][1]
-                print '\nRecall: ', recall_score(y_true, preds)
+                print 'Recall: ', recall_score(y_true, preds)
                 print 'Precision: ', precision_score(y_true, preds)
                 print 'Accuracy: ', accuracy_score(y_true, preds)
                 if classifier != 'log':
@@ -257,9 +274,85 @@ def oversample_train_test(df, y):
     return X_train_over, X_test, y_train_over, y_test
 
 def my_fbeta(estimator, X, y):
-    beta = 10
+    beta = .5
     preds = estimator.predict(X)
     return fbeta_score(y, preds, beta)
+
+def change_beta(new_beta):
+    global beta
+    beta = new_beta
+
+def more_precise_grid(df, y):
+        X_train, X_test, y_train, y_test = oversample_train_test(df, y)
+
+        prej_model = Pipeline([
+            ('lists', ListSplitter()),
+            ('race', RaceDummies()),
+            ('crime_sentence', CrimeAndSentence()),
+            ('feat_eng', FeatureEngineer()),
+            ('columns', ColumnFilter()),
+            ('est', PickEstimator())
+        ])
+
+        no_prej_model = Pipeline([
+            ('lists', ListSplitter()),
+            ('race', RaceDummies()),
+            ('crime_sentence', CrimeAndSentence()),
+            ('feat_eng', FeatureEngineer()),
+            ('columns', ColumnFilter(prejudice=False)),
+            ('est', PickEstimator())
+        ])
+
+        for esto in [LogisticRegression(), AdaBoostClassifier(), GradientBoostingClassifier()]:
+            prej_model.set_params(est__estimator=esto)
+            no_prej_model.set_params(est__estimator=esto)
+
+            print '\nPrejudice:\n'
+            # prej_grid = {'gbc__learning_rate': [1.25, 1, 0.75],
+            #              'gbc__n_estimators': [750]}
+            #
+            # gs_prej = GridSearchCV(prej_model, prej_grid, scoring=my_fbeta, verbose=1)
+            # gs_prej.fit(X_train.copy(), y_train)
+            # print '\nCV FBeta: ', gs_prej.best_score_
+            # print gs_prej.best_params_
+            prej_model.fit(X_train.copy(), y_train)
+            prej_preds = prej_model.predict(X_test.copy())
+            prej_scores = prej_model.predict_proba(X_test.copy())[:,1]
+            print 'Proba Range: {}-{}'.format(prej_scores.min(), prej_scores.max())
+            print 'Recall: ', recall_score(y_test, prej_preds)
+            print 'Precision: ', precision_score(y_test, prej_preds)
+            print 'Accuracy: ', accuracy_score(y_test, prej_preds)
+            plot_pr_curve(prej_scores, y_test, 'Prej, {}'.format(esto.__class__.__name__))
+
+            print '\nNo Prejudice:\n'
+
+            no_prej_grid = {'gbc__learning_rate': [0.07, 0.05, 0.04],
+                            'gbc__n_estimators': [75, 80, 85]}
+
+            # gs_no_prej = GridSearchCV(no_prej_model, no_prej_grid, scoring=my_fbeta, verbose=1)
+            # gs_no_prej.fit(X_train.copy(), y_train)
+            # print '\nCV FBeta: ', gs_no_prej.best_score_
+            # print gs_no_prej.best_params_
+            no_prej_model.fit(X_train.copy(), y_train)
+            no_prej_preds = no_prej_model.predict(X_test.copy())
+            no_prej_scores = no_prej_model.predict_proba(X_test.copy())[:,1]
+            print 'Proba Range: {}-{}'.format(no_prej_scores.min(), no_prej_scores.max())
+            print 'Recall: ', recall_score(y_test, no_prej_preds)
+            print 'Precision: ', precision_score(y_test, no_prej_preds)
+            print 'Accuracy: ', accuracy_score(y_test, no_prej_preds)
+
+            plot_pr_curve(no_prej_scores, y_test, 'No Prej, {}'.format(esto.__class__.__name__))
+
+        plt.legend(loc='best')
+        plt.show()
+
+def plot_pr_curve(probas, y_test, label):
+    precision, recall, thresholds = precision_recall_curve(y_test, probas)
+    plt.plot(recall, precision, label=label)
+
+def pr_auc(estimator, X, y):
+    probas = estimator.predict_proba(X.copy())
+
 
 
 
@@ -279,71 +372,10 @@ if __name__ == '__main__':
     ])
 
 
-
-    # Find out which training_sets and classifiers get the best recall.
-    training_sets, test_sets = get_train_test_sets(feature_engineering, df, y)
-    best_score, best_estimator, all_ests = big_grid_search(training_sets, test_sets)
-
-    print '\nPrejudice: \n'
-    prej_est = best_estimator['prej']
-    prej_y = test_sets['prej'][1]
-    prej_preds = prej_est.predict(test_sets['prej'][0])
-    print 'Recall: ', recall_score(prej_y, prej_preds)
-    print 'Precision: ', precision_score(prej_y, prej_preds)
-    print 'Accuracy: ', accuracy_score(prej_y, prej_preds)
-
-    print '\nNo Prejudice: \n'
-    prej_est = best_estimator['noprej']
-    prej_y = test_sets['noprej'][1]
-    prej_preds = prej_est.predict(test_sets['noprej'][0])
-    print 'Recall: ', recall_score(prej_y, prej_preds)
-    print 'Precision: ', precision_score(prej_y, prej_preds)
-    print 'Accuracy: ', accuracy_score(prej_y, prej_preds)
-
-    with open('models.pkl', 'w') as f:
-        pickle.dump(best_estimator, f)
+    # Find out which training_sets and classifiers get the best fbeta.
+    # training_sets, test_sets = get_train_test_sets(feature_engineering, df, y)
+    # best_score, best_estimator, all_ests = big_grid_search(training_sets, test_sets)
 
 
-    ## More specific grid_search
-    # X_train, X_test, y_train, y_test = oversample_train_test(df, y)
-    #
-    # prej_model = Pipeline([
-    #     ('lists', ListSplitter()),
-    #     ('race', RaceDummies()),
-    #     ('crime_sentence', CrimeAndSentence()),
-    #     ('feat_eng', FeatureEngineer()),
-    #     ('columns', ColumnFilter()),
-    #     ('rf', RandomForestClassifier(n_jobs=-1))
-    # ])
-    #
-    # no_prej_model = Pipeline([
-    #     ('lists', ListSplitter()),
-    #     ('race', RaceDummies()),
-    #     ('crime_sentence', CrimeAndSentence()),
-    #     ('feat_eng', FeatureEngineer()),
-    #     ('columns', ColumnFilter(prejudice=False)),
-    #     ('ada', AdaBoostClassifier())
-    # ])
-    #
-    # print '\nPrejudice:\n'
-    # prej_grid = {'rf__criterion': ['gini', 'entropy'],
-    #              'rf__n_estimators': [200, 225, 250, 275, 300]}
-    #
-    # gs_prej = GridSearchCV(prej_model, prej_grid, scoring='recall')
-    # gs_prej.fit(X_train.copy(), y_train)
-    # print gs_prej.best_params_
-    # prej_preds = gs_prej.best_estimator_.predict(X_test.copy())
-    # print 'Recall: ', recall_score(y_test, prej_preds)
-    # print 'Precision: ', precision_score(y_test, prej_preds)
-    #
-    # print '\nNo Prejudice:\n'
-    #
-    # no_prej_grid = {'ada__learning_rate': [0.05, 0.01, 0.005, 0.001],
-    #                 'ada__n_estimators': [25, 50, 75, 100]}
-    #
-    # gs_no_prej = GridSearchCV(no_prej_model, no_prej_grid, scoring='recall')
-    # gs_no_prej.fit(X_train.copy(), y_train)
-    # print gs_no_prej.best_params_
-    # no_prej_preds = gs_no_prej.best_estimator_.predict(X_test.copy())
-    # print 'Recall: ', recall_score(y_test, no_prej_preds)
-    # print 'Precision: ', precision_score(y_test, no_prej_preds)
+    # More specific grid_search
+    more_precise_grid(df, y)
